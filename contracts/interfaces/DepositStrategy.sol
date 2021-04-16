218f3da2a336cd3135819db0b9b766b39f896ef2
shubh singh <ssi@rapidinnovation.dev>
2021-04-16 11:39:25 +0530

    added tripool strategy

--- contracts/interfaces/DepositStrategy.sol
+++
@@ -1,8 +0,0 @@
-//SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.7.6;
-
-interface DepositStrategy{
-    function deposit(uint256[10] memory amounts) external returns(uint256);
-    function withdraw(uint256[10] memory amounts) external returns(uint256);
-    function setRewardCoin(address rewardCoin) external returns(bool);
-}
--- contracts/interfaces/UniswapRouter.sol
+++ contracts/interfaces/UniswapRouter.sol
@@ -1,8 +1,9 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.7.6;
 
-interface  UniswapV2Router02  {
+interface  UniswapV2Router  {
     function swapExactTokensForETH(uint, uint, address[] calldata, address, uint) external  returns (uint[] memory);
     function getAmountsOut(uint, address[] calldata) external view returns (uint[] memory);
     function WETH() external pure returns (address); 
+    function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to,uint deadline) external returns (uint[] memory amounts);
 }
